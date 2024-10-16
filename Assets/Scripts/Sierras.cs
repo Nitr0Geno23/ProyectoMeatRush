a73014ff01bb751b4f92a5effd57bbcf71036644
Nitr0Geno23 <nilortegaromera@gmail.com>
2024-10-16 14:33:31 +0200

    Sierras recien empezadas
    pues eso

---
+++ Assets/Scripts/Sierras.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sierras : MonoBehaviour
+{
+    public Player player;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            player.Death();
+            collision.gameObject.SetActive(false);
+
+        }
+
+    }
+
+}
