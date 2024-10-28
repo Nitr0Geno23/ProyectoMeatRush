a9565ee9cd4230343c6e9f9ab28105ad69d42f01
Nitr0Geno23 <nilortegaromera@gmail.com>
2024-10-28 14:30:08 +0100

    Arte
    Texturas hechas con shadders

--- Assets/Scripts/Divider.cs
+++ Assets/Scripts/Divider.cs
@@ -1,52 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Divider : MonoBehaviour
-{
-    public GameObject clonePlayer; 
+//public class Divider : MonoBehaviour
+//{
+//    public GameObject clonePlayer; 
 
-    void Start()
-    {
-        if (clonePlayer != null)
-        {
-            clonePlayer.SetActive(false);
-        }
-    }
+//    void Start()
+//    {
+//        if (clonePlayer != null)
+//        {
+//            clonePlayer.SetActive(false);
+//        }
+//    }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player") && clonePlayer != null)
-        {
-            Debug.Log("Activando el clon del jugador con gravedad invertida.");
+//    void OnTriggerEnter(Collider other)
+//    {
+//        if (other.CompareTag("Player") && clonePlayer != null)
+//        {
+//            Debug.Log("Activando el clon del jugador con gravedad invertida.");
 
            
-            clonePlayer.SetActive(true);
-            Player clonePlayerScript = clonePlayer.GetComponent<Player>();
-            clonePlayerScript.SetDuplicateBehavior(); 
+//            clonePlayer.SetActive(true);
+//            Player clonePlayerScript = clonePlayer.GetComponent<Player>();
+//            clonePlayerScript.SetDuplicateBehavior(); 
 
 
-            Player playerScript = other.GetComponent<Player>();
-            playerScript.Death += HandlePlayerDeath;
-            clonePlayerScript.Death += HandlePlayerDeath;
-        }
-    }
+//            Player playerScript = other.GetComponent<Player>();
+//            playerScript.Death += HandlePlayerDeath;
+//            clonePlayerScript.Death += HandlePlayerDeath;
+//        }
+//    }
 
-    private void HandlePlayerDeath(Player deadPlayer)
-    {
-        if (deadPlayer.CompareTag("Player"))
-        {
-            Debug.Log("Jugador original ha muerto. Desactivando el clon y reiniciando.");
+//    private void HandlePlayerDeath(Player deadPlayer)
+//    {
+//        if (deadPlayer.CompareTag("Player"))
+//        {
+//            Debug.Log("Jugador original ha muerto. Desactivando el clon y reiniciando.");
 
-            // Desactivar el clon y respawnear al jugador original
-            clonePlayer.SetActive(false);
-            deadPlayer.Respawn();
-        }
-        else if (deadPlayer.CompareTag("DuplicatePlayer"))
-        {
-            Debug.Log("El clon ha muerto. Desactivando y esperando reactivación.");
-            clonePlayer.SetActive(false);
-        }
-    }
-}
+//            // Desactivar el clon y respawnear al jugador original
+//            clonePlayer.SetActive(false);
+//            deadPlayer.Respawn();
+//        }
+//        else if (deadPlayer.CompareTag("DuplicatePlayer"))
+//        {
+//            Debug.Log("El clon ha muerto. Desactivando y esperando reactivación.");
+//            clonePlayer.SetActive(false);
+//        }
+//    }
+//}
 
